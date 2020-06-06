b3c69cb374f9b6311674cfbe36753544a9d11cb9
DrNegativ <65175682+DrNegativ@users.noreply.github.com>
2020-06-06 16:12:41 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var button = document.getElementById('gta5');
+button.addEventListener('click', function () {
+  alert('Downloading gta5');
+});
+var button = document.getElementById('gta_san');
+button.addEventListener('click', function () {
+  alert('Downloading GTA San Andreas');
+});
+var button = document.getElementById('sims');
+button.addEventListener('click', function () {
+  alert('Downloading SIMS4');
+});
+var button = document.getElementById('GTA4');
+button.addEventListener('click', function () {
+  alert('Downloading GTA4');
+});
+var button = document.getElementById('minecraft');
+button.addEventListener('click', function () {
+  alert('Downloading Minecraft');
+});
+var button = document.getElementById('gta5_2');
+button.addEventListener('click', function () {
+  alert('Downloading gta5');
+});
+var button = document.getElementById('san');
+button.addEventListener('click', function () {
+  alert('Downloading GTA San Andreas');
+});
+var button = document.getElementById('sims4');
+button.addEventListener('click', function () {
+  alert('Downloading SIMS4');
+});
+var button = document.getElementById('gta4');
+button.addEventListener('click', function () {
+  alert('Downloading GTA4');
+});
+var button = document.getElementById('Minecraft');
+button.addEventListener('click', function () {
+  alert('Downloading Minecraft');
+});
+var button = document.getElementById('cross');
+button.addEventListener('click', function () {
+  alert('Downloading Crossaut');
+});
+var button = document.getElementById('metr');
+button.addEventListener('click', function () {
+  alert('Downloading Metro Exodus');
+});
+var button = document.getElementById('sky');
+button.addEventListener('click', function () {
+  alert('Downloading SkyRim');
+});
+var button = document.getElementById('maf');
+button.addEventListener('click', function () {
+  alert('Downloading Mafia 3');
+});
+var button = document.getElementById('speed');
+button.addEventListener('click', function () {
+  alert('Downloading Need for Speed Most Wanted 2005');
+});
+var button = document.getElementById('combat');
+button.addEventListener('click', function () {
+  alert('Downloading Mortal Combat X');
+});
+var button = document.getElementById('fort');
+button.addEventListener('click', function () {
+  alert('Downloading Fortnite');
+});
+var button = document.getElementById('pc');
+button.addEventListener('click', function () {
+  alert('Downloading PC Building Simulator');
+});
+var button = document.getElementById('eu');
+button.addEventListener('click', function () {
+  alert('Downloading Euru');
+});
+var button = document.getElementById('hello');
+button.addEventListener('click', function () {
+  alert('Downloading Hello Neighbor');
+});
+
+//reg
+var button = document.getElementById('reg');
+button.addEventListener('click', function () {
+	
+});
+
+//log
+var button = document.getElementById('log');
+button.addEventListener('click', function () {
+	
+});
+
+
